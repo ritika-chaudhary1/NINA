883a4a9724ac56215231599496166af90ab952de
ritika-chaudhary1 <ritikachaudhary419@gmail.com>
2025-08-21 13:38:29 +0545

    index file updated

--- resources/views/service/index.blade.php
+++ resources/views/service/index.blade.php
@@ -7,8 +7,8 @@
     <div class="container text-center">
         <h1 class="mb-2">All Services</h1>
         <p>
-            <a href="{{ url('/') }}" class="text-white text-decoration-none">Home</a> /
-            <span>Services</span>
+            <a href="{{ url('/') }}" class="text-white text-decoration-none">Home</a> 
+       
         </p>
     </div>
 </section>
